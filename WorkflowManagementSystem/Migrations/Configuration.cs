55b338dc514e07c9bc142043f2b7d032467c6a8a
Joey <j.lai@live.ca>
2015-02-09 13:39:48 -0700

    Seed data on DB initialization

--- WorkflowManagementSystem/Migrations/Configuration.cs
+++ WorkflowManagementSystem/Migrations/Configuration.cs
@@ -1,12 +1,13 @@
-using System.ComponentModel.DataAnnotations.Schema;
+using WorkflowManagementSystem.Models;
+using WorkflowManagementSystem.Models.ApprovalChains;
 using WorkflowManagementSystem.Models.DataAccess;
+using WorkflowManagementSystem.Models.Faculty;
+using WorkflowManagementSystem.Models.Roles;
+using WorkflowManagementSystem.Models.Semesters;
 
 namespace WorkflowManagementSystem.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WorkflowManagementSystemDbContext>
     {
@@ -20,16 +21,34 @@ namespace WorkflowManagementSystem.Migrations
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // ROLES
+            FacadeFactory.GetDomainFacade().CreateRole(new RoleInputViewModel("Faculty Member"));
+            FacadeFactory.GetDomainFacade().CreateRole(new RoleInputViewModel("Faculty Council Member"));
+            FacadeFactory.GetDomainFacade().CreateRole(new RoleInputViewModel("Faculty Curriculumn Member"));
+            FacadeFactory.GetDomainFacade().CreateRole(new RoleInputViewModel("APPC Member"));
+            FacadeFactory.GetDomainFacade().CreateRole(new RoleInputViewModel("GFC Member"));
+
+            // FACULTIES
+            FacadeFactory.GetDomainFacade().CreateFaculty(new FacultyInputViewModel { Name = "Bissett School of Business" });
+            FacadeFactory.GetDomainFacade().CreateFaculty(new FacultyInputViewModel { Name = "Science and Technology" });
+
+            // DISCIPLINES
+            FacadeFactory.GetDomainFacade().CreateDiscipline(new DisciplineInputViewModel { Code = "MKTG", Faculty = "Bissett School of Business", Name = "Marketing" });
+            FacadeFactory.GetDomainFacade().CreateDiscipline(new DisciplineInputViewModel { Code = "MGMT", Faculty = "Bissett School of Business", Name = "Management" });
+            FacadeFactory.GetDomainFacade().CreateDiscipline(new DisciplineInputViewModel{ Code = "COMP", Faculty = "Science and Technology", Name = "Computer Science" });
+
+            // SEMESTERS
+            FacadeFactory.GetDomainFacade().CreateSemester(new SemesterInputViewModel { Term = "Fall", Year = "2014" });
+            FacadeFactory.GetDomainFacade().CreateSemester(new SemesterInputViewModel{ Term = "Winter", Year = "2015"});
+            FacadeFactory.GetDomainFacade().CreateSemester(new SemesterInputViewModel{ Term = "Spring", Year = "2015"});
+            FacadeFactory.GetDomainFacade().CreateSemester(new SemesterInputViewModel{ Term = "Fall", Year = "2015"});
+            FacadeFactory.GetDomainFacade().CreateSemester(new SemesterInputViewModel{ Term = "Winter", Year = "2016"});
+            FacadeFactory.GetDomainFacade().CreateSemester(new SemesterInputViewModel{ Term = "Spring", Year = "2016"});
+
+            // APPROVAL CHAINS
+            FacadeFactory.GetDomainFacade().CreateApprovalChain(
+                new ApprovalChainInputViewModel { Name = "Program", Roles = {"Faculty Council Member","Faculty Curriculumn Member","APPC Member", "GFC Member"} }
+            );
         }
     }
 }
