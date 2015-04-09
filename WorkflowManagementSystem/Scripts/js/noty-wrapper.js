de544ef13861f2100b9ad0e4a89d9310171408fb
Joey <j.lai@live.ca>
2015-04-08 23:36:52 -0600

    Added notifications

---
+++ WorkflowManagementSystem/Scripts/js/noty-wrapper.js
@@ -0,0 +1,33 @@
+﻿function showSuccessNotification(text) {
+    noty({
+        layout: "center",
+        theme: "relax",
+        type: "success",
+        text: text,
+        timeout: 2000,
+        buttons: false,
+        animation: {
+            open: "animated fadeIn", // Animate.css class names
+            close: "animated fadeOut", // Animate.css class names
+            easing: "swing",
+            speed: 500
+        }
+    });
+}
+
+function showErrorNotification(text) {
+    noty({
+        layout: "center",
+        theme: "relax",
+        type: "error",
+        text: text,
+        timeout: 2000,
+        buttons: false,
+        animation: {
+            open: "animated fadeIn", // Animate.css class names
+            close: "animated fadeOut", // Animate.css class names
+            easing: "swing",
+            speed: 500
+        }
+    });
+}
