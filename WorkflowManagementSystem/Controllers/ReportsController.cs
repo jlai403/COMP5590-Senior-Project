5328a3b70109250c274add4c12686a3292f20775
Joey <j.lai@live.ca>
2015-04-26 19:15:01 -0600

    PDF Report

---
+++ WorkflowManagementSystem/Controllers/ReportsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using RazorPDF;
+using WorkflowManagementSystem.Models;
+
+namespace WorkflowManagementSystem.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public ActionResult Course(string name)
+        {
+            var course = FacadeFactory.GetDomainFacade().FindCourse(name);
+            return new PdfActionResult(course);
+        }
+	}
+}
